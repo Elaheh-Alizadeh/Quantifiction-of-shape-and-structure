74635d82fc4a884da82b7b60216c1b4e12995752
Elaheh Alizadeh <elaheh.alizade@gmail.com>
2020-04-08 15:01:07 -0700

    Update StartHere.m

--- StartHere.m
+++ StartHere.m
@@ -10,7 +10,8 @@ Nuc_Binary  = imread(fullfile(in_dir, BWNuc_File));
 Nuc_Binary2  = imread(fullfile(in_dir, BWNuc2_File));
 Cell_Gray = imread(fullfile(in_dir, GrayCell_File));
 PolarOrCartesian='Polar';
-[Features,FeatureNames]=CalculateParameters(Cell_Binary,Nuc_Binary,Nuc_Binary2,Cell_Gray,0,30,35)
+[Features,FeatureNames]=CalculateParameters(Cell_Binary,Nuc_Binary,Nuc_Binary2,Cell_Gray,0,30,35)% First & second number: Zernike moments orders. L
+% The last number is number of fourier series. To test the code use 0,1,1 to have faster evaluation. 
 
 All=[Features.Geometric,Features.GrayScale,Features.WavinessRT,Features.WavinessXY,Features.RoughnessRT,Features.RoughnessXY,Features.FractalDimension,Features.BandBased,Features.ZernikeMoment];
 AllNames=[FeatureNames.Geometric,FeatureNames.GrayScale,FeatureNames.WavinessRT,FeatureNames.WavinessXY,FeatureNames.RoughnessRT,FeatureNames.RoughnessXY,FeatureNames.FractalDimension,FeatureNames.BandBased,FeatureNames.ZernikeMoment];
